300ac7a38f170fa2772f3171943a8f2370704e20
Stephane <39770957+Caractarus@users.noreply.github.com>
2018-05-30 20:19:37 +0200

    Classe Article : Ajout des commentaires DocBlocks

--- Article.php
+++ Article.php
@@ -2,18 +2,31 @@
 
 class Article
 {
+     /**
+     * @var string $title Describe the title data
+     */
     private $title;
 
+    /**
+     * @param string $title 
+     */
     public function __construct($title)
     {
         $this->title = $title;
     }
-
+    
+    /**
+     * @param string obtenir le titre
+     * return string
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     * @param string déterminer le titre
+     */
     public function setTitle()
     {
         $this->title = $title;
